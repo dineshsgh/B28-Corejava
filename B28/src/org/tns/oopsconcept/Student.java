dc8535ef85b22238c5dc96fe3293e3cd6400061d
ASUS <ASUS@LAPTOP-K79MPD51>
2022-10-15 12:13:42 -0700

    first commit

---
+++ B28/src/org/tns/oopsconcept/Student.java
@@ -0,0 +1,11 @@
+package org.tns.oopsconcept;
+
+public class Student {
+	Student(){
+		System.out.println("Student object is created");
+	}
+	public static void main(String args[])
+	{
+		Student ob =new Student();
+	}
+}
